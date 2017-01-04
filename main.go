34d2f7ec8854eee711782cc8878bd479eadf4b14
Derek Ahn <git.derek@gmail.com>
2017-01-03 16:31:43 -0800

    Wiring up search query to API

--- main.go
+++ main.go
@@ -7,7 +7,10 @@ import (
 
 	"database/sql"
 	"encoding/json"
+	"encoding/xml"
 	_ "github.com/mattn/go-sqlite3"
+	"io/ioutil"
+	"net/url"
 )
 
 type Page struct {
@@ -16,10 +19,10 @@ type Page struct {
 }
 
 type SearchResult struct {
-	Title  string
-	Author string
-	Year   string
-	ID     string
+	Title  string `xml:"title,attr"`
+	Author string `xml:"author,attr"`
+	Year   string `xml:"hyr,attr"`
+	ID     string `xml:"owi,attr"`
 }
 
 func main() {
@@ -44,18 +47,42 @@ func main() {
 	})
 
 	http.HandleFunc("/search", func(w http.ResponseWriter, r *http.Request) {
-		results := []SearchResult{
-			SearchResult{"Moby-Dick", "Herman Melville", "1851", "222222"},
-			SearchResult{"The Adventures of Huckleberry Finn", "Mark Twain", "1884", "444444"},
-			SearchResult{"The Catcher in the Rye", "JD Salinger", "1951", "333333"},
+		var results []SearchResult
+		var err error
+
+		if results, err = search(r.FormValue("search")); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
 
 		encoder := json.NewEncoder(w)
 		if err := encoder.Encode(results); err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
-		db.Close()
 	})
 
 	fmt.Println(http.ListenAndServe(":3000", nil))
+}
+
+type ClassifySearchResponse struct {
+	Results []SearchResult `xml:"works>work"`
+}
+
+func search(query string) ([]SearchResult, error) {
+	var resp *http.Response
+	var err error
+
+	source := "http://classify.oclc.org/classify2/Classify?&summary=true&title="
+	if resp, err = http.Get(source + url.QueryEscape(query)); err != nil {
+		return []SearchResult{}, err
+	}
+
+	defer resp.Body.Close()
+	var body []byte
+	if body, err = ioutil.ReadAll(resp.Body); err != nil {
+		return []SearchResult{}, err
+	}
+
+	var c ClassifySearchResponse
+	err = xml.Unmarshal(body, &c)
+	return c.Results, err
 }
