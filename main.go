e25f872c46f63df46af26fed045f8c9f8da7ec40
Derek Ahn <git.derek@gmail.com>
2017-01-03 17:14:01 -0800

    Added the route handler for /books/add

--- main.go
+++ main.go
@@ -61,6 +61,21 @@ func main() {
 		}
 	})
 
+	http.HandleFunc("/books/add", func(w http.ResponseWriter, r *http.Request) {
+		var book ClassifyBookResponse
+		var err error
+
+		if book, err = find(r.FormValue("id")); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+		if err = db.Ping(); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+
+		_, err = db.Exec("insert into books (pk, title, author, id, classification) values (?, ?, ?, ?, ?)",
+			nil, book.BookData.Title, book.BookData.Author, book.BookData.ID, book.Classification.MostPopular)
+	})
+
 	fmt.Println(http.ListenAndServe(":3000", nil))
 }
 
